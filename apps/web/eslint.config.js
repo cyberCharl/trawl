f6212f4117cb63a245f35d594ec21cb4c44c40ba
Charl <charlbotha21@gmail.com>
2026-04-14 17:31:54 +0200

    chore: clear repo before scaffold import

--- apps/web/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { nextJsConfig } from "@trawl/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
--- apps/web/postcss.config.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "@trawl/ui/postcss.config";
